19097d2c5a4562807775aeb554bef6abb7503e0a
Zhen Luo <461652354@qq.com>
2025-10-16 20:25:28 +0800

    conways: fixed directory structure

--- conways_game_of_life/godot/scene/world/world.gd
+++ conways_game_of_life/godot/scene/world/world.gd
@@ -1,3 +1,4 @@
+class_name World
 extends Node2D
 
 const PLAY_ICON := preload("res://asset/play-button.svg")
@@ -12,7 +13,7 @@ enum Tool {
 var _playing := false:
 	set = set_playing
 var _last_step_start := 0.0
-var _step_interval := 1000.0
+var _step_interval := 0.0
 
 var _current_tool := Tool.None:
 	set = set_current_tool
@@ -29,12 +30,33 @@ var _tool_drag_last = null
 	%PencilButton,
 	%BucketButton,
 ]
+@onready var zoom_spin_box: SpinBox = %ZoomSpinBox
+@onready var play_back_limit_spin_box: SpinBox = %PlaybackLimitSpinBox
 
 
 func _ready() -> void:
-	grid.reset(Vector2i(32, 32))
 	for button in tool_buttons:
 		assert(button.toggle_mode)
+	var new_popup: PopupMenu = %NewButton.get_popup()
+	new_popup.id_pressed.connect(_on_new_popup_id_pressed)
+	grid.size_changed.connect(func(sz): %GridSizeLabel.text = "Grid Size: %d x %d" % [sz.x, sz.y])
+	grid.generation_changed.connect(func(gen): %GenerationLabel.text = "Generation: " + str(gen))
+	# grid.data_changed.connect(
+	# 	# OPTIMIZE:
+	# 	func(data: PackedByteArray):
+	# 		var n_alive = 0
+	# 		for byte in data:
+	# 			n_alive += int(byte != 0)
+	# 		%CounterLabel.text = "Alive: " + str(n_alive)
+	# )
+	grid.cell_size_changed.connect(func(sz): zoom_spin_box.value = sz)
+	zoom_spin_box.value_changed.connect(func(v): grid.cell_size = v)
+	play_back_limit_spin_box.value_changed.connect(func(v): _step_interval = 1e6 / v)
+	_step_interval = 1e6 / play_back_limit_spin_box.value
+	grid.position = Global.VIEWPORT_SIZE / 2.0
+	grid.reset(Vector2i(32, 32))
+	grid.cell_size = 20.0
+	%ToolPreview.grid = grid
 
 
 func _process(_delta: float) -> void:
@@ -43,13 +65,13 @@ func _process(_delta: float) -> void:
 		if now - _last_step_start > _step_interval:
 			_last_step_start = now
 			grid.step()
-			%GenerationLabel.text = "Generation: %d" % grid.get_generation()
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		_current_mouse_cell_pos = grid.global_to_cell_pos(event.global_position)
+		_current_mouse_cell_pos = grid.global_to_cell_pos(get_global_mouse_position())
 		if _current_tool != Tool.None:
+			%ToolPreview.cell_pos = _current_mouse_cell_pos
 			if _tool_drag_last != null and _tool_drag_last != _current_mouse_cell_pos and event.button_mask & _tool_last_clicked_button:
 				_tool_drag_last = _current_mouse_cell_pos
 				if _tool_last_clicked_button == MOUSE_BUTTON_LEFT:
@@ -73,17 +95,18 @@ func _unhandled_input(event: InputEvent) -> void:
 				_tool_drag_last = _tool_last_clicked_pos
 				_tool_apply_right_click(_tool_last_clicked_pos)
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.is_pressed():
-			var zoom_center = Global.VIEWPORT_SIZE / 2.0
-			var zoom_center_cell_pos = grid.global_to_cell_pos(zoom_center)
-			grid.cell_size = minf(100.0, grid.cell_size * 1.25)
-			var zoom_center_new = grid.cell_to_global_pos(zoom_center_cell_pos)
-			grid.global_position += zoom_center - zoom_center_new
+			grid.zoom_at(grid.cell_size * 1.25, event.global_position)
+			%ToolPreview.cell_pos = grid.global_to_cell_pos(event.global_position)
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.is_pressed():
-			var zoom_center = Global.VIEWPORT_SIZE / 2.0
-			var zoom_center_cell_pos = grid.global_to_cell_pos(zoom_center)
-			grid.cell_size = maxf(1.0, grid.cell_size / 1.25)
-			var zoom_center_new = grid.cell_to_global_pos(zoom_center_cell_pos)
-			grid.global_position += zoom_center - zoom_center_new
+			grid.zoom_at(grid.cell_size / 1.25, event.global_position)
+			%ToolPreview.cell_pos = grid.global_to_cell_pos(event.global_position)
+		if event.button_index == MOUSE_BUTTON_MIDDLE and event.is_pressed() and event.double_click:
+			grid.position = Global.VIEWPORT_SIZE / 2.0
+
+	elif event.is_action_type():
+		if event.is_action(&"zoom_reset"):
+			grid.position = Global.VIEWPORT_SIZE / 2.0
+			grid.zoom_at(10.0, Global.VIEWPORT_SIZE / 2.0)
 
 
 func set_playing(value: bool):
@@ -100,6 +123,7 @@ func set_playing(value: bool):
 
 func set_current_tool(value: Tool):
 	_current_tool = value
+	%ToolPreview.tool = _current_tool
 	if _current_tool == Tool.None:
 		_tool_last_clicked_pos = null
 		_tool_drag_start = null
@@ -162,7 +186,6 @@ func _on_next_button_pressed() -> void:
 	if _playing:
 		return
 	grid.step()
-	%GenerationLabel.text = "Generation: %d" % grid.get_generation()
 
 
 func _on_play_pause_button_pressed() -> void:
@@ -180,5 +203,39 @@ func _on_bucket_button_toggled(toggled_on: bool) -> void:
 
 
 func _on_trash_button_pressed() -> void:
+	grid.position = Global.VIEWPORT_SIZE / 2.0
 	grid.reset()
 	_playing = false
+
+
+func _on_new_popup_id_pressed(id: int) -> void:
+	grid.position = Global.VIEWPORT_SIZE / 2.0
+	match id:
+		0:
+			grid.reset(Vector2i(32, 32))
+			grid.cell_size = 20.0
+		1:
+			grid.reset(Vector2i(128, 128))
+			grid.cell_size = 5.0
+		2:
+			grid.reset(Vector2i(1024, 1024))
+			grid.cell_size = 1.0
+		3:
+			grid.reset(Vector2i(4096, 4096))
+			grid.cell_size = 1.0
+
+
+func _on_playback_limit_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		%PlaybackLimitCheckBox.self_modulate = Color.WHITE
+		play_back_limit_spin_box.editable = true
+		_step_interval = 1e6 / play_back_limit_spin_box.value
+	else:
+		%PlaybackLimitCheckBox.self_modulate = Color.DARK_GRAY
+		play_back_limit_spin_box.editable = false
+		_step_interval = 0.0
+
+
+func _on_randomize_button_pressed() -> void:
+	grid.position = Global.VIEWPORT_SIZE / 2.0
+	grid.randomize(0.5)
--- conways_game_of_life/scene/global.gd
+++
@@ -1,3 +0,0 @@
-class_name Global
-
-const VIEWPORT_SIZE := Vector2(1000.0, 1000.0)
