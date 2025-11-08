83263bf0942e10e1b9dfc696e52b038b8876443a
Zhen Luo <461652354@qq.com>
2025-11-09 01:54:53 +0800

    connect-four: solver wip

--- EX-connet_four/rust/src/lib.rs
+++ EX-connet_four/rust/src/lib.rs
@@ -6,28 +6,54 @@ pub(crate) mod position;
 pub(crate) mod solver;
 pub(crate) mod transposition;
 
+use position::Position;
+
 #[derive(GodotClass)]
 #[class(base=Node)]
 struct C4Solver {
+    solver: solver::Solver,
     base: Base<Node>,
 }
 #[godot_api]
 impl INode for C4Solver {
     fn init(base: Base<Node>) -> Self {
         godot_print!("Hello, world!");
-        Self { base }
+        Self {
+            solver: Default::default(),
+            base,
+        }
     }
 }
 
 #[godot_api]
 impl C4Solver {
     #[func]
-    fn solve() -> i32 {
-        todo!()
+    fn solve(&mut self, moves: PackedByteArray) -> i32 {
+        let mut p = Position::default();
+        for col in moves.as_slice() {
+            p.play(*col as usize)
+        }
+        self.solver.solve(&p, false)
+    }
+
+    #[func]
+    fn rate_next_moves(&mut self, moves: PackedByteArray) -> Array<Variant> {
+        let mut p = Position::default();
+        for col in moves.as_slice() {
+            p.play(*col as usize)
+        }
+        let mut arr = Array::<Variant>::new();
+        arr.resize(Position::WIDTH, &Variant::nil());
+        for col in 0..Position::WIDTH {
+            if p.can_play(col) {
+                let mut newp = p;
+                newp.play(col);
+                arr.set(col, &self.solver.solve(&p, false).to_variant());
+            }
+        }
+        arr
     }
 }
-
-impl C4Solver {}
 
 struct MyExtension;
 
--- EX-connet_four/rust/src/position.rs
+++ EX-connet_four/rust/src/position.rs
@@ -1,226 +1,47 @@
-use std::fmt::Display;
-use std::fmt::Write;
-use std::str::FromStr;
-
-pub(crate) trait Position {
-    const WIDTH: usize = 7;
-    const HEIGHT: usize = 6;
-    // NOTE: all columns below are 0-based
-
-    /// Whether a column is playable, i.e. not yet full.
-    fn can_play(&self, col: usize) -> bool;
-    /// Play at a column, assuming it's playable.
-    fn play(&mut self, col: usize);
-    /// Whether the current player wins by playing at the given column, assuming it's playable.
-    fn is_winning_move(&self, col: usize) -> bool;
-    /// Number of moves played since the beginning of the game.
-    fn n_moves(&self) -> usize;
-    /// Transposition table key
-    fn key(&self) -> u64;
-}
-
 #[derive(Default, Clone, Copy)]
-pub(crate) struct ArrayPosition {
-    pieces: [[i8; 6]; 7],
-    n: usize,
-}
-impl Position for ArrayPosition {
-    fn can_play(&self, col: usize) -> bool {
-        self.pieces[col][5] == 0
-    }
-
-    fn play(&mut self, col: usize) {
-        let cur = if self.n.is_multiple_of(2) { 1 } else { 2 };
-        for x in &mut self.pieces[col] {
-            if *x == 0 {
-                *x = cur;
-                self.n += 1;
-                return;
-            }
-        }
-    }
-
-    fn is_winning_move(&self, col: usize) -> bool {
-        let cur = if self.n.is_multiple_of(2) { 1 } else { 2 };
-        let x = col;
-        let y = {
-            let mut y = 0usize;
-            for i in 0..6 {
-                if self.pieces[x][i] == 0 {
-                    y = i;
-                    break;
-                }
-            }
-            y
-        };
-
-        if y >= 3
-            && self.pieces[x][y - 1] == cur
-            && self.pieces[x][y - 2] == cur
-            && self.pieces[x][y - 3] == cur
-        {
-            return true;
-        }
-
-        {
-            let mut l = 0;
-            for i in 1..4 {
-                if x >= i && self.pieces[x - i][y] == cur {
-                    l += 1;
-                } else {
-                    break;
-                }
-            }
-            let mut r = 0;
-            for i in 1..4 {
-                if x + i < 7 && self.pieces[x + i][y] == cur {
-                    r += 1;
-                } else {
-                    break;
-                }
-            }
-            if l + r >= 3 {
-                return true;
-            }
-        }
-        {
-            let mut tl = 0;
-            for i in 1..4 {
-                if x >= i && y + i < 6 && self.pieces[x - i][y + i] == cur {
-                    tl += 1;
-                } else {
-                    break;
-                }
-            }
-            let mut br = 0;
-            for i in 1..4 {
-                if x + i < 7 && y >= i && self.pieces[x + i][y - i] == cur {
-                    br += 1;
-                } else {
-                    break;
-                }
-            }
-            if tl + br >= 3 {
-                return true;
-            }
-        }
-        {
-            let mut bl = 0;
-            for i in 1..4 {
-                if x >= i && y >= i && self.pieces[x - i][y - i] == cur {
-                    bl += 1;
-                } else {
-                    break;
-                }
-            }
-            let mut tr = 0;
-            for i in 1..4 {
-                if x + i < 7 && y + i < 6 && self.pieces[x + i][y + i] == cur {
-                    tr += 1;
-                } else {
-                    break;
-                }
-            }
-            if bl + tr >= 3 {
-                return true;
-            }
-        }
-        false
-    }
-
-    fn n_moves(&self) -> usize {
-        self.n
-    }
-
-    fn key(&self) -> u64 {
-        unimplemented!()
-    }
-}
-impl Display for ArrayPosition {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for y in (0..6).rev() {
-            for x in 0..7 {
-                let p = self.pieces[x][y];
-                let c = match p {
-                    0 => ' ',
-                    1 => '1',
-                    2 => '2',
-                    _ => unreachable!(),
-                };
-                f.write_char(c)?;
-            }
-            f.write_char('\n')?;
-        }
-        Ok(())
-    }
-}
-impl FromStr for ArrayPosition {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut ret = Self::default();
-        for c in s.chars() {
-            let d = c.to_digit(10).ok_or_else(|| "invalid digit".to_string())? as usize;
-            if !ret.can_play(d) {
-                return Err("invalid play".to_string());
-            }
-            ret.play(d);
-        }
-        Ok(ret)
-    }
-}
-impl ArrayPosition {
-    pub(crate) fn to_bit_position(self) -> BitPosition {
-        let cur = if self.n.is_multiple_of(2) { 1 } else { 2 };
-        let mut position = 0i64;
-        let mut mask = 0i64;
-        for x in 0..7 {
-            for y in 0..6 {
-                let p = self.pieces[x][y];
-                if p != 0 {
-                    mask |= 1 << (y + x * 7);
-                    if p == cur {
-                        position |= 1 << (y + x * 7);
-                    }
-                }
-            }
-        }
-        let moves = self.n;
-        BitPosition {
-            position,
-            mask,
-            moves,
-        }
-    }
-}
-
-/*
- *
- * .  .  .  .  .  .  .
- * 5 12 19 26 33 40 47
- * 4 11 18 25 32 39 46
- * 3 10 17 24 31 38 45
- * 2  9 16 23 30 37 44
- * 1  8 15 22 29 36 43
- * 0  7 14 21 28 35 42
- */
-#[derive(Default, Clone, Copy)]
-pub(crate) struct BitPosition {
+pub(crate) struct Position {
     position: i64,
     mask: i64,
     moves: usize,
 }
-impl BitPosition {
-    // const BOTTOM: i64 = 0b_0000001_0000001_0000001_0000001_0000001_0000001_0000001;
-    fn top_mask(col: usize) -> i64 {
+impl Position {
+    pub(crate) const WIDTH: usize = 7;
+    pub(crate) const HEIGHT: usize = 6;
+
+    /* bits layout:
+     * .  .  .  .  .  .  .
+     * 5 12 19 26 33 40 47
+     * 4 11 18 25 32 39 46
+     * 3 10 17 24 31 38 45
+     * 2  9 16 23 30 37 44
+     * 1  8 15 22 29 36 43
+     * 0  7 14 21 28 35 42
+     */
+
+    const BOTTOM_MASK: i64 = Self::bottom_mask(0)
+        | Self::bottom_mask(1)
+        | Self::bottom_mask(2)
+        | Self::bottom_mask(3)
+        | Self::bottom_mask(4)
+        | Self::bottom_mask(5)
+        | Self::bottom_mask(6);
+    const BOARD_MASK: i64 = Self::column_mask(0)
+        | Self::column_mask(1)
+        | Self::column_mask(2)
+        | Self::column_mask(3)
+        | Self::column_mask(4)
+        | Self::column_mask(5)
+        | Self::column_mask(6);
+    const fn top_mask(col: usize) -> i64 {
         1 << (col * 7 + 5)
     }
-    fn bottom_mask(col: usize) -> i64 {
+    const fn bottom_mask(col: usize) -> i64 {
         1 << (col * 7)
     }
-    fn column_mask(col: usize) -> i64 {
+    const fn column_mask(col: usize) -> i64 {
         0b111111 << (col * 7)
     }
-    fn has_wins(pos: i64) -> bool {
+    const fn has_wins(pos: i64) -> bool {
         let m = pos & (pos >> 7);
         if m & (m >> 14) != 0 {
             return true;
@@ -239,29 +60,92 @@ impl BitPosition {
         }
         false
     }
-}
-impl Position for BitPosition {
-    fn can_play(&self, col: usize) -> bool {
+    fn find_winning_moves(position: i64, mask: i64) -> i64 {
+        let HEIGHT = Self::HEIGHT;
+
+        // vertical;
+        let mut r = (position << 1) & (position << 2) & (position << 3);
+
+        //horizontal
+        let mut p = (position << (HEIGHT + 1)) & (position << (2 * (HEIGHT + 1)));
+        r |= p & (position << (3 * (HEIGHT + 1)));
+        r |= p & (position >> (HEIGHT + 1));
+        p = (position >> (HEIGHT + 1)) & (position >> (2 * (HEIGHT + 1)));
+        r |= p & (position << (HEIGHT + 1));
+        r |= p & (position >> (3 * (HEIGHT + 1)));
+
+        //diagonal 1
+        p = (position << HEIGHT) & (position << (2 * HEIGHT));
+        r |= p & (position << (3 * HEIGHT));
+        r |= p & (position >> HEIGHT);
+        p = (position >> HEIGHT) & (position >> (2 * HEIGHT));
+        r |= p & (position << HEIGHT);
+        r |= p & (position >> (3 * HEIGHT));
+
+        //diagonal 2
+        p = (position << (HEIGHT + 2)) & (position << (2 * (HEIGHT + 2)));
+        r |= p & (position << (3 * (HEIGHT + 2)));
+        r |= p & (position >> (HEIGHT + 2));
+        p = (position >> (HEIGHT + 2)) & (position >> (2 * (HEIGHT + 2)));
+        r |= p & (position << (HEIGHT + 2));
+        r |= p & (position >> (3 * (HEIGHT + 2)));
+
+        return r & (Self::BOARD_MASK ^ mask);
+    }
+
+    pub(crate) fn can_play(&self, col: usize) -> bool {
         self.mask & Self::top_mask(col) == 0
     }
 
-    fn play(&mut self, col: usize) {
+    pub(crate) fn play(&mut self, col: usize) {
         self.position ^= self.mask;
         self.mask |= self.mask + Self::bottom_mask(col);
         self.moves += 1;
     }
 
-    fn is_winning_move(&self, col: usize) -> bool {
-        let mut p = self.position;
-        p |= (self.mask + Self::bottom_mask(col)) & Self::column_mask(col);
-        Self::has_wins(p)
+    pub(crate) fn is_winning_move(&self, col: usize) -> bool {
+        self.winning_moves() & self.possible_moves() & Self::column_mask(col) != 0
     }
 
-    fn n_moves(&self) -> usize {
+    pub(crate) fn n_moves(&self) -> usize {
         self.moves
     }
 
-    fn key(&self) -> u64 {
+    pub(crate) fn key(&self) -> u64 {
         (self.position + self.mask) as u64
+    }
+
+    pub(crate) fn possible_moves(&self) -> i64 {
+        (self.mask + Self::BOTTOM_MASK) & Self::BOARD_MASK
+    }
+    pub(crate) fn possible_non_losing_moves(&self) -> i64 {
+        let mut possible_moves = self.possible_moves();
+        let opponent_winning_moves = self.opponent_winning_moves();
+        let forced_moves = possible_moves & opponent_winning_moves;
+        if forced_moves != 0 {
+            if forced_moves & (forced_moves - 1) != 0 {
+                // more than one forced moves, will always lose
+                return 0;
+            }
+            possible_moves = forced_moves;
+        }
+        possible_moves & !(opponent_winning_moves >> 1)
+    }
+    pub(crate) fn winning_moves(&self) -> i64 {
+        Self::find_winning_moves(self.position, self.mask)
+    }
+    pub(crate) fn opponent_winning_moves(&self) -> i64 {
+        Self::find_winning_moves(self.position ^ self.mask, self.mask)
+    }
+    pub(crate) fn can_win_next(&self) -> bool {
+        self.winning_moves() & self.possible_moves() != 0
+    }
+
+    pub(crate) fn apply_str(&mut self, s: &str) {
+        for c in s.chars() {
+            let col = c.to_digit(10).expect("valid digit") as usize;
+            assert!(self.can_play(col));
+            self.play(col);
+        }
     }
 }
